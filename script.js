89920ea9dae61fde07c9e06c4377aa42983e59ea
Diego Varady <varaday@hotmail.com>
2021-02-28 15:08:24 -0300

    Finished

--- script.js
+++ script.js
@@ -3,23 +3,19 @@ const slideOne = document.getElementById("txt-img1"),
 
 slideTwo.style.display = "none";
 
-slideOne.addEventListener("click", change);
-slideTwo.addEventListener("click", change);
-document.addEventListener("keyup", changeKey);
+document.addEventListener("click", change);
+document.addEventListener("keyup", change);
 
-function change(e) {    
-    if (slideOne.style.display === "none") {
-        slideOne.style.display = "";
-        slideTwo.style.display = "none";
-    } else if (slideTwo.style.display === "none") {
-        slideOne.style.display = "none";
-        slideTwo.style.display = "";
-    }
-}
-
-function changeKey(e) {
-    let x = e.key;
-    if (x === "ArrowRight" || x === "ArrowLeft") {
-        change();
+function change(e) {
+    let y = e.target;
+    let x = e.key;    
+    if (y.id === "Btn" || x === "ArrowRight" || x === "ArrowLeft") {
+        if (slideOne.style.display === "none") {
+            slideOne.style.display = "";
+            slideTwo.style.display = "none";
+        } else if (slideTwo.style.display === "none") {
+            slideOne.style.display = "none";
+            slideTwo.style.display = "";
+        }
     }    
 }
